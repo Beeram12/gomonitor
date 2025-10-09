92f0bcfb79ecd0dc6048971cbc3402a549068e65
Beeram Pranith Reddy <pranithreddy16.beeram@gmail.com>
2025-10-09 16:11:21 +0530

    Add views for TUI

---
+++ internal/tui/view.go
@@ -0,0 +1,65 @@
+package tui
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/charmbracelet/lipgloss"
+)
+
+func (m model) View() string {
+	if m.quitting {
+		return "Shutting down...\n"
+	}
+	switch m.currentView {
+	case addView:
+		return m.viewAdd()
+	case removeView:
+		return m.viewRemove()
+	default:
+		return m.viewDashboard()
+	}
+}
+
+func (m model) viewDashboard() string {
+	var b strings.Builder
+
+	b.WriteString("***GoMonitor is Live***\n\n")
+
+	header := lipgloss.NewStyle().Bold(true).Render(fmt.Sprintf("%-50s | %s", "URL", "STATUS"))
+
+	b.WriteString(header + "\n")
+	b.WriteString(strings.Repeat("-", 65) + "\n")
+
+	for _, url := range m.urls {
+		status, ok := m.statuses[url]
+		statusStr := "Checking...."
+		style := lipgloss.NewStyle().Foreground(lipgloss.Color("245"))
+
+		if ok {
+			if status.IsUp {
+				statusStr = fmt.Sprintf("UP (Code: %d)", status.StatusCode)
+				style = lipgloss.NewStyle().Foreground(lipgloss.Color("40"))
+			} else {
+				statusStr = fmt.Sprintf("DOWN (Code: %d)", status.StatusCode)
+				style = lipgloss.NewStyle().Foreground(lipgloss.Color("196"))
+			}
+		}
+		b.WriteString(fmt.Sprintf("%-50s | %s\n", url, style.Render(statusStr)))
+	}
+
+	help := "\n'a' to add | 'r' to remove | 'q' to quit\n"
+	b.WriteString(help)
+	return b.String()
+}
+
+func (m model) viewAdd() string {
+	return fmt.Sprintf(
+		"Enter the new website URL to monitor:\n\n%s\n\n(esc to cancel)",
+		m.textInput.View(),
+	)
+}
+
+func (m model) viewRemove() string {
+	return "\n" + m.list.View()
+}
--- pkg/checker/checker.go
+++ pkg/checker/checker.go
@@ -6,8 +6,9 @@ import (
 )
 
 type Status struct {
-	URL  string
-	IsUp bool
+	URL        string
+	IsUp       bool
+	StatusCode int
 }
 
 func CheckEndPoint(url string) Status {
@@ -17,21 +18,24 @@ func CheckEndPoint(url string) Status {
 	resp, err := client.Get(url)
 	if err != nil {
 		return Status{
-			URL:  url,
-			IsUp: false,
+			URL:        url,
+			IsUp:       false,
+			StatusCode: 0,
 		}
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
 		return Status{
-			URL:  url,
-			IsUp: true,
+			URL:        url,
+			IsUp:       true,
+			StatusCode: resp.StatusCode,
 		}
 	}
 
 	return Status{
-		URL:  url,
-		IsUp: false,
+		URL:        url,
+		IsUp:       false,
+		StatusCode: resp.StatusCode,
 	}
 }
