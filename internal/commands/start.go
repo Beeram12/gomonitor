2cc6137aea727e4250f348024267246710741e0e
Beeram Pranith Reddy <pranithreddy16.beeram@gmail.com>
2025-10-12 00:09:49 +0530

    Add start function

--- internal/commands/start.go
+++ internal/commands/start.go
@@ -1,14 +1,30 @@
 package commands
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"log"
+
+	"github.com/Beeram12/gomonitor/internal/tui"
+	"github.com/Beeram12/gomonitor/pkg/config"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/spf13/cobra"
+)
 
 var startCmd = &cobra.Command{
 	Use:   "start",
 	Short: "Starts the interactive monitoring dashboard",
 	Run: func(cmd *cobra.Command, args []string) {
-		/*
-			Code for interacting with TUI
-		*/
+		cfg, err := config.LoadConfig()
+		if err != nil {
+			log.Fatalf("Could not load config: %v", err)
+		}
+
+		initialModel := tui.InitialModel(cfg.URLs)
+		p := tea.NewProgram(initialModel)
+		tui.Program = p
+		if _, err := p.Run(); err != nil {
+			fmt.Printf("Error running program: %v\n", err)
+		}
 	},
 }
 
